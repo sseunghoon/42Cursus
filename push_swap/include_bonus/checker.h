25279b25b47dc1f04ff85b6999ef1629d820fdaf
Seunghun Song <seunghso@c2r3s6.42seoul.kr>
2022-08-09 23:02:01 +0900

    checker start

--- push_swap/include_bonus/checker.h
+++ push_swap/include_bonus/checker.h
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   checker.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunghso <seunghso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/28 23:29:53 by seunghso          #+#    #+#             */
-/*   Updated: 2022/07/28 23:29:53 by seunghso         ###   ########.fr       */
+/*   Created: 2022/08/09 22:29:46 by seunghso          #+#    #+#             */
+/*   Updated: 2022/08/09 22:29:46 by seunghso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#ifndef CHECKER_H
+# define CHECKER_H
 
+# include "../libft/include/get_next_line.h"
 # include "../libft/include/ft_printf.h"
 # include "../libft/include/libft.h"
 
@@ -33,9 +34,7 @@ typedef struct t_deque
 
 // push_swap.c
 void	parse_argument(t_deque *a, t_deque *b, int argc, char **argv);
-void	divide_3part(t_deque *a, t_deque *b, int pivot1, int pivot2);
-void	sort_just3(t_deque *a);
-void	greedy(t_deque *a, t_deque *b);
+void	init_deque(t_deque *a, t_deque *b);
 
 // command.c
 void	swap(t_deque *d);
@@ -54,23 +53,8 @@ void	add_bot(t_deque *x, t_node *new_node);
 t_node	*pop_top(t_deque *x);
 t_node	*pop_bot(t_deque *x);
 
-// greedy.c
-void	get_best_loc(t_deque *a, t_deque *b, int *dest, int *sttp);
-int		is_better(int tmp_dest, int tmp_sttp, int dest, int sttp);
-void	greedy_rotate(t_deque *a, t_deque *b, int dest, int sttp);
-void	last_rotate(t_deque *a);
-int		find_dest(t_deque *a, t_node *src);
-
 // parse.c
-void	change_to_idx(t_deque *a);
 void	receive_input(t_deque *a, int argc, char **argv);
 void	append_data(t_deque *x, int data);
-int		binary_search(int *arr, int data, int left, int right);
-void	quick_sort(int *arr, int left, int right);
-
-// etc.c
-void	ft_swap(int *a, int *b);
-void	init_arr(int **arr, int **dup_ck, t_deque *a);
-void	init_deque(t_deque *a, t_deque *b);
 
 #endif
---
+++ push_swap/src_bonus/parse.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghso <seunghso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 23:18:59 by seunghso          #+#    #+#             */
+/*   Updated: 2022/07/28 23:18:59 by seunghso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include_bonus/checker.h"
+#include <stdio.h>
+
+void	receive_input(t_deque *a, int argc, char **argv)
+{
+	char	**p;
+	char	**save_p;
+	int		i;
+
+	if (argc == 2)
+	{
+		p = ft_split(argv[1], ' ');
+		save_p = p;
+		while (*p)
+		{
+			append_data(a, ft_atoi(*p));
+			free(*p);
+			p++;
+		}
+		free(save_p);
+	}
+	else
+	{
+		i = 0;
+		while (++i < argc)
+			append_data(a, ft_atoi(argv[i]));
+	}
+}
+
+void	append_data(t_deque *x, int data)
+{
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	new_node->data = data;
+	if (!(x->top))
+	{
+		x->top = new_node;
+		x->bot = new_node;
+	}
+	else
+	{
+		x->bot->next = new_node;
+		new_node->prev = x->bot;
+		new_node->next = x->top;
+		x->top->prev = new_node;
+		x->bot = new_node;
+	}
+	x->size++;
+}
