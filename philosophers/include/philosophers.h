55ccacb65d5b9a5ab9c522c62fbc635538c13577
Seunghun Song <seunghso@c3r7s6.42seoul.kr>
2023-02-07 17:31:48 +0900

    monitoring() 에서 info 가 초기화 되어있지 않다고 뜨는데 왜 그런지 안 보임

--- philosophers/include/philosophers.h
+++ philosophers/include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: seunghso <seunghso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 18:40:21 by seunghso          #+#    #+#             */
-/*   Updated: 2023/02/06 19:33:44 by seunghso         ###   ########.fr       */
+/*   Updated: 2023/02/07 15:57:13 by seunghso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,31 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <string.h>
 
 # define USING 1
 # define NOT_USING 0
 
-typedef struct	s_philo_info
-{
-	pthread_t		thread;
-	int				last_eat_time;
-	int				number_eat;
-	pthread_mutex_t	*mutex;
-}	t_philo_info;
-
 typedef struct	s_simul_info
 {
-	int				number_of_philosophers;
+	int				number_of_philos;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				number_must_eat;
 	int				*forks;
 	pthread_mutex_t	mutex;
-	t_philo_info	*philos;
 }	t_simul_info;
+
+typedef struct	s_philo_info
+{
+	int				number_philo;
+	int				last_eat_time;
+	int				number_eat;
+	int				left;
+	int				right;
+	pthread_t		thread;
+	t_simul_info	*t_simul_info;
+}	t_philo_info;
 
 #endif
--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -6,7 +6,7 @@
 /*   By: seunghso <seunghso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 18:42:08 by seunghso          #+#    #+#             */
-/*   Updated: 2023/02/06 20:26:18 by seunghso         ###   ########.fr       */
+/*   Updated: 2023/02/07 17:30:45 by seunghso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,94 +35,163 @@ int	ft_atoi(const char *str)
 	return (result);
 }
 
-void *life_cycle(void *info)
+long	get_time()
 {
+	struct	timeval time;
+
+	gettimeofday(&time, NULL);
+	return time.tv_usec;
+}
+
+int	join_thread(pthread_t *monitor, t_philo_info *philos)
+{
+	pthread_join(*monitor, NULL);
+	while (philos->number_philo--)
+		pthread_join(philos->thread, NULL);
+	return (0);
+}
+
+void	*monitoring(void *philosophers)
+{
+	int				i;
+	int				num_philos;
+	t_simul_info	*info;
+	t_philo_info	*philos;
+
+	philos = (t_philo_info *)philosophers;
+	printf("debug0\n");
+
+	info = philos[0].t_simul_info;
+	printf("debug0.5\n");
+
+	num_philos = info->number_of_philos;
+	printf("debug1\n");
+	while (1)
+	{
+		i = 0;
+		while (i < num_philos)
+		{
+			printf("debug2\n");
+			if (philos[i].last_eat_time + info->time_to_die > get_time())
+			{
+				printf("%d: Die\n", i);
+			}
+			i++;
+		}
+
+	}
+	return NULL;
+}
+
+int	create_monitor(pthread_t *monitor, t_philo_info *philos)
+{
+	printf("create monitor\n");
+	pthread_create(monitor, NULL, monitoring, philos);
+
+	return 0;
+}
+
+int	take_fork(t_philo_info *philo, t_simul_info *simul_info)
+{
+	if (simul_info->forks[philo->right] == NOT_USING)
+	{
+		simul_info->forks[philo->right] = USING;
+		if (simul_info->forks[philo->left] == NOT_USING)
+			simul_info->forks[philo->left] = USING;
+		else
+		{
+			printf("I have right and not have left\n");
+			return 0;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+void	putdown_fork(t_philo_info *philo, t_simul_info *simul_info)
+{
+	simul_info->forks[philo->right] = NOT_USING;
+	simul_info->forks[philo->left] = NOT_USING;
+}
+
+void	*life_cycle(void *philosopher)
+{
+	t_philo_info	*philo;
 	t_simul_info	*simul_info;
 
-	simul_info = (t_simul_info *)info;
+	philo = (t_philo_info *)philosopher;
+	simul_info = philo->t_simul_info;
 	while (1)
 	{
 		pthread_mutex_lock(&simul_info->mutex);
-		write(1, "life_cycle\n", 11);
-		sleep(3);
-		write(1, "unlock\n", 7);
+		int success = take_fork(philo, simul_info);
+		if (success)
+		{
+			philo->last_eat_time = get_time();
+			printf("%d %d: Eat\n", philo->last_eat_time, philo->number_philo);
+			usleep(simul_info->time_to_eat * 1000);
+			putdown_fork(philo, simul_info);
+		}
 		pthread_mutex_unlock(&simul_info->mutex);
+		printf("%d: Sleep\n", philo->number_philo);
+		usleep(simul_info->time_to_sleep * 1000);
+		printf("%d: Thinking\n", philo->number_philo);
 	}
 	
 	return NULL;
 }
 
-int	create_philosophers(t_simul_info *info)
+int	create_philosophers(t_simul_info *info, t_philo_info *philos)
 {
-	int				num_philo;
-	t_philo_info	*philos;
+	int	i;
 
-	if (pthread_mutex_init(&info->mutex, NULL) < 0)
-		return -2;
-	num_philo = info->number_of_philosophers;
-	info->philos = malloc(sizeof(t_philo_info) * num_philo);
-	philos = info->philos;
-	if (philos->thread == NULL)
+	i = 0;
+	while (++i < info->number_of_philos)
 	{
-		printf("Error to create pthread\n");
-		return -1;
-	}
-	while (num_philo > 0)
-	{
-		philos[num_philo-1].mutex = &info->mutex;
-		pthread_create(&philos[num_philo-1].thread, NULL, life_cycle, info);
-		
-		num_philo--;
+		philos[i].number_philo = i;
+		philos[i].number_eat = 0;
+		philos[i].last_eat_time = 0;
+		philos[i].t_simul_info = info;
+		philos[i].left = (info->number_of_philos + i - 1) % info->number_of_philos;
+		philos[i].right = (info->number_of_philos + i) % info->number_of_philos;
+		pthread_create(&philos[i].thread, NULL, life_cycle, &philos[i]);
 	}
 	return 0;
 }
 
-int	init_simul_info(t_simul_info *info, int argc, char **argv)
+int	init_simul(t_philo_info **philos, t_simul_info *info, int argc, char **argv)
 {
-	int	i;
-	info->number_of_philosophers = ft_atoi(argv[1]);
+	if (pthread_mutex_init(&info->mutex, NULL) < 0)
+		return -2;
+	info->number_of_philos = ft_atoi(argv[1]);
 	info->time_to_die = ft_atoi(argv[2]);
 	info->time_to_eat = ft_atoi(argv[3]);
 	info->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
 		info->number_must_eat = ft_atoi(argv[5]);
-	info->forks = malloc(sizeof(int) * info->number_of_philosophers);
-	if (info->forks == NULL)
+	*philos = malloc(sizeof(t_philo_info) * info->number_of_philos);
+	info->forks = malloc(sizeof(int) * info->number_of_philos);
+	if (*philos == NULL || info->forks == NULL)
 		return -1;
-	i = 0;
-	while (i < info->number_of_philosophers)
-		info->forks[i++] = NOT_USING;
-	create_philosophers(info);
-	return 0;
-}
-
-int	detach_join(t_simul_info *info)
-{
-	// 에러 처리 할 것
-	int				i;
-	t_philo_info	*philos;
-
-	philos = info->philos;
-
-	i = 0;
-	while (i < info->number_of_philosophers)
-	{
-		pthread_join(philos[i].thread, NULL);
-	}
+	memset(info->forks, NOT_USING, info->number_of_philos * sizeof(int));
 	return 0;
 }
 
 int	main(int argc, char **argv)
 {
+	t_philo_info	*philos;
 	t_simul_info	simul_info;
+	pthread_t		monitor;
 
 	if (argc != 5 && argc != 6)
 	{
 		printf("Invalid Input\n");
 		return -1;
 	}
-	init_simul_info(&simul_info, argc, argv);
-	detach_join(&simul_info);
+	init_simul(&philos, &simul_info, argc, argv);
+	create_philosophers(&simul_info, philos);
+	create_monitor(&monitor, philos);
+	join_thread(&monitor, philos);
 
 	return 0;
 }
