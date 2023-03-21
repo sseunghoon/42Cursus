63a75c365c3a28b4538895c728fd43156cebe8d4
Seunghun Song <seunghso@c3r3s1.42seoul.kr>
2023-03-21 16:24:19 +0900

    ex01 ~

--- cpp_module/00/ex01/phonebook.cpp
+++ cpp_module/00/ex01/phonebook.cpp
@@ -1,31 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunghso <seunghso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 14:06:22 by seunghso          #+#    #+#             */
-/*   Updated: 2023/03/21 16:11:29 by seunghso         ###   ########.fr       */
+/*   Updated: 2023/03/21 16:23:59 by seunghso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+#include "phonebook.hpp"
+
+void	add(PhoneBook )
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
+	PhoneBook[10]	phoneBook;
+	std::string	command;
+	
+	
+	while (1)
 	{
-		for (int i = 1; i < argc; i++)
-		{
-			std::string str(argv[i]);
-			for (size_t j = 0; j < str.length(); j++)
-				std::cout << (char)std::toupper(str[j]);
-		}
-		std::cout << std::endl;
+		std::cout << "Command:";
+		std::cin >> command;
+		if (command.compare("EXIT"))
+			break ;
+		if (command.compare("ADD"))
+			add();
+		if (command.compare("SEARCH"))
+			search();
 	}
 	return (0);
 }
--- cpp_module/00/ex01/phonebook.hpp
+++ cpp_module/00/ex01/phonebook.hpp
@@ -1,31 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunghso <seunghso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/21 14:06:22 by seunghso          #+#    #+#             */
-/*   Updated: 2023/03/21 16:11:29 by seunghso         ###   ########.fr       */
+/*   Created: 2023/03/21 14:50:06 by seunghso          #+#    #+#             */
+/*   Updated: 2023/03/21 16:22:11 by seunghso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int	main(int argc, char *argv[])
+class PhoneBook
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
-	{
-		for (int i = 1; i < argc; i++)
-		{
-			std::string str(argv[i]);
-			for (size_t j = 0; j < str.length(); j++)
-				std::cout << (char)std::toupper(str[j]);
-		}
-		std::cout << std::endl;
-	}
-	return (0);
+	private:
+		int		index;
+		char	first_name[10];
+		char	last_name[10];
+		char	nick_name[10];
+		char	phone_number[10];
+		char	darkest_secret[10];
+	public:
+		void	print_self();
 }
